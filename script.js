6b9edbf9f28f7140d74720af28840364a2f0acba
giabaosoftware <82156969+giabaosoftware@users.noreply.github.com>
2024-08-27 19:21:32 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -75,10 +75,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Chức năng chụp và lưu trữ hình ảnh
     captureButton.addEventListener('click', function() {
-        const dataURL = canvas.toDataURL('image/jpeg'); // Thay đổi định dạng từ PNG sang JPEG
+        const dataURL = canvas.toDataURL('image/png');
         const link = document.createElement('a');
         link.href = dataURL;
-        link.download = 'hinh_canvas.jpeg'; // Đặt tên file là .jpeg
+        link.download = 'hinh_canvas.png';
         link.click();
     });
 
